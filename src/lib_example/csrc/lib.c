ae4403bf970c4ff60047ddf570ff4478091f65f5
Shubham Ingale <shubhamingale779@gmail.com>
2025-02-17 12:43:46 +0000

    c_func: added c function to get temprature which returns a const array

---
+++ src/lib_example/csrc/lib.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "const_array.h"
+
+const float* get_temprature(int* size) {
+    static const float arr[] = {261.59f, 286.23f, 234.3f, 342.4f, 352.5f};
+    *size = sizeof(arr) / sizeof(arr[0]);
+    return arr;
+}
---
+++ src/lib_example/csrc/lib.h
@@ -0,0 +1,4 @@
+#ifndef SHGI_LIB_H
+#define SHGI_LIB_H
+const float* get_temprature(int* size);
+#endif
