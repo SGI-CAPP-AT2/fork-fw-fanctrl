f07dad0a732060110c27f395c9ec12ec1289e18a
Shubham Ingale <shubhamingale779@gmail.com>
2025-02-18 03:20:40 +0000

    move_fix: setup.py errors fixed

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ import os
 
 csrc_dir = os.path.abspath("./csrc")
 
-shgi_mod = Extension(  
-    "shgi_mod",  
+shgi_mod = Extension(
+    "lib_example.shgi_mod",  # Note the dotted name here!
     sources=["src/lib_example/src/wrapper.pyx", "src/lib_example/csrc/lib.c"],
     include_dirs=[csrc_dir],
 )
 
 setup(
-    ext_modules=cythonize(shgi_mod),  
+    ext_modules=cythonize(shgi_mod),
     include_dirs=[csrc_dir],
+    package_dir={'': 'src'}, # Tell setuptools where the packages are
+    packages=['lib_example'], # Important: List the package(s)
 )
