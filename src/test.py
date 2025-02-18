ff95e09027e239675530a0c98da9f10770fd487b
Shubham Ingale <shubhamingale779@gmail.com>
2025-02-18 02:32:56 +0000

    doc: description for test added

--- src/test.py
+++ src/test.py
@@ -1,3 +1,6 @@
+"""
+This test is added for quick check without installation of fw_fanctrl
+"""
 from fw_fanctrl.hardwareController.EctoolHardwareController import EctoolHardwareController
 
 ectoolhwc = EctoolHardwareController(no_battery_sensor_mode=False)
